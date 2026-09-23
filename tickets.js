673434aceda9f88327de839a0be756fcc5c9d7ed
kerbygomezabistado <kerbygomezabistado@gmail.com>
2026-09-23 20:20:38 +0800

    Add 50% VIP surcharge for premium seating - abistado.kerby

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.round(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, isVIP = false) {
+  let totalPrice = quantity * basePrice;
+
+  if (isVIP) {
+    totalPrice = totalPrice * 1.50;
+  }
+
+  return Math.floor(totalPrice);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
